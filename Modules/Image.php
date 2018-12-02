a0bd0cf6f79ff178551460f3cd6aaa9f2006c3ce
Denis Tokarev <den4@me.com>
2018-12-02 11:24:50 +0300

    lesson 14 img refactoring

--- Modules/Image.php
+++ Modules/Image.php
@@ -2,16 +2,14 @@
 
 class Image
 {
-    public function upload($img)
+    public function upload($img) // for example /tmp/dfkhkj.jpg
     {
-        $fileName = $img["name"];
-        $fileTmpLoc = $img["tmp_name"];
-        $kaboom = explode(".", $fileName);
+        $kaboom = explode(".", $img);
         $fileExt = end($kaboom);
         $uploaded_name = rand(100000000000,999999999999) . "." . $fileExt;
         $imgFolderLocation = ROOT . 'usercontent/uploads/';
         $uploadFile = $imgFolderLocation . $uploaded_name;
 
-        return move_uploaded_file($fileTmpLoc, $uploadFile);
+        return move_uploaded_file($img, $uploadFile);
     }
 }
