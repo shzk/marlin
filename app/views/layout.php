a8a6154a150018e0098a1204f69a7cdcb895fc98
Denis Tokarev <den4@me.com>
2018-12-02 22:42:42 +0300

    lesson 24 Kint

---
+++ app/views/layout.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title><?=$this->e($title)?></title>
+</head>
+<body>
+sdsd
+<?=$this->section('content')?>
+
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -2,8 +2,15 @@
 
 require '../vendor/autoload.php';
 
-if ($_SERVER['REQUEST_URI'] === '/home') {
-    require '../app/controllers/homepage.php';
-}
+// Create new Plates instance
+$templates = new League\Plates\Engine('../app/views');
+d($templates);
 
-exit;
+// Render a template
+echo $templates->render('homepage', ['name' => 'Jonathan']);
+
+//if ($_SERVER['REQUEST_URI'] === '/home') {
+////    require '../app/controllers/homepage.php';
+////}
+////
+////exit;
