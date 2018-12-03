ce2fec0cc0a64939c4f80f1353e974fc5d2ba4f5
Denis Tokarev <den4@me.com>
2018-12-03 21:46:24 +0300

    lesson 27 Routing

--- public/index.php
+++ public/index.php
@@ -6,20 +6,36 @@ if( !session_id() ) @session_start();
 
 require '../vendor/autoload.php';
 
-if (true) {
-    flash()->message('Hot!');
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/users', 'get_all_users_handler');
+    // {id} must be a number (\d+)
+    $r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+    // The /{title} suffix is optional
+    $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
 }
+$uri = rawurldecode($uri);
 
-echo flash()->display();
-// Create new Plates instance
-$templates = new League\Plates\Engine('../app/views');
-d($templates);
-
-// Render a template
-echo $templates->render('homepage', ['name' => 'Jonathan']);
-
-//if ($_SERVER['REQUEST_URI'] === '/home') {
-////    require '../app/controllers/homepage.php';
-////}
-////
-////exit;
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+        break;
+}
